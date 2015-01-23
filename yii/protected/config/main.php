75e0c54548e39e2bcdb8a338093941eafac7fd26
sujit510 <sujit510@gmail.com>
2015-01-23 19:08:43 +0530

    added code to remove the index.php from URL

--- yii/protected/config/main.php
+++ yii/protected/config/main.php
@@ -41,12 +41,19 @@ return array(
 		// uncomment the following to enable URLs in path-format
 		
 		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
+//			'urlFormat'=>'path',
+//			'rules'=>array(
+//				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+//				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+//				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+//			),
+                        'showScriptName'=>false,
+                        'urlFormat'=>'path',
+                        'rules'=>array(
+                        '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                        '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                        '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                    ),
 		),
 		
 
